6eb6d98133c02aa616225b6b768b0b3147125531
maggieoneill <margaretanneoneill@gmail.com>
2015-10-20 10:55:04 -0700

    finished enrollment thank you message and userinput.

--- app/src/main/java/com/epicodus/breakthecode/ui/EnrollActivity.java
+++ app/src/main/java/com/epicodus/breakthecode/ui/EnrollActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,7 +24,28 @@ public class EnrollActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enroll);
 
-        mEmailText = (EditText);
+        mEmailText = (EditText) findViewById(R.id.emailText);
+        mNameText = (EditText) findViewById(R.id.nameText);
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+        mThankyouText = (TextView) findViewById(R.id.thankyouLabel);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mNameText.getText().toString();
+                String email = mEmailText.getText().toString();
+                String message = String.format(
+                        "Hello %s, thank you for your interest in Break the Code! \n\n " +
+                                "We have sent more information by email to %s", name, email
+                );
+                mThankyouText.setText(message);
+
+                mEmailText.setVisibility(View.INVISIBLE);
+                mNameText.setVisibility(View.INVISIBLE);
+                mSubmitButton.setVisibility(View.INVISIBLE);
+                mThankyouText.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 
